bb7a3d6b7913c87c476ec772bfb224d5f6b2d786
moretime-dev <moretimeproductions@googlemail.com>
2022-05-20 18:26:00 +0200

    first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="TopCenter">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl text-white"
+                style={{ background: "blue" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
