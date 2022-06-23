897c82b6c625744b84577de638625d46f89e7b84
moretime-dev <moretimeproductions@googlemail.com>
2022-06-23 14:50:27 +0200

    test commit

--- src/pages/Ecommerce.tsx
+++ src/pages/Ecommerce.tsx
@@ -34,31 +34,31 @@ const DropDown: React.FC<{ currentMode: string }> = ({ currentMode }) => (
 const Ecommerce = () => {
   const { currentColor, currentMode } = useStateContext();
   return (
-    <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
-          <div className="flex justify-between items-center">
-            <div>
-              <p className="font-bold text-gray-400">Earnings</p>
-              <p className="text-2xl">$63,448.78</p>
-            </div>
-            <button
-              type="button"
-              style={{ backgroundColor: currentColor }}
-              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
-            >
-              <BsCurrencyDollar />
-            </button>
+    <div className="mt-12">
+      <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="font-bold text-gray-400">Earnings</p>
+            <p className="text-2xl">$63,448.78</p>
           </div>
-          <div className="mt-6">
-            <Button
-              color="white"
-              bgColor={currentColor}
-              text="Download"
-              borderRadius="10px"
-            />
-          </div>
+          <button
+            type="button"
+            style={{ backgroundColor: currentColor }}
+            className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+          >
+            <BsCurrencyDollar />
+          </button>
         </div>
+        <div className="mt-6">
+          <Button
+            color="white"
+            bgColor={currentColor}
+            text="Download"
+            borderRadius="10px"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
           {earningData.map((item) => (
             <div
@@ -83,7 +83,6 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
-
       <div className="flex gap-10 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
           <div className="flex justify-between">
@@ -191,7 +190,6 @@ const Ecommerce = () => {
           </div>
         </div>
       </div>
-
       <div className="flex gap-10 m-4 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
           <div className="flex justify-between items-center gap-2">
@@ -244,7 +242,6 @@ const Ecommerce = () => {
           </div>
         </div>
       </div>
-
       <div className="flex flex-wrap justify-center">
         <div className="md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
           <div className="flex justify-between">
